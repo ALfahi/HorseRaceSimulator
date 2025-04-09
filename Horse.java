df38a64c5f57a100ced28540c152a813ca3a4305
Fahi Sabab, Al <feha.tasnim27@gmail.com>
2025-04-09 15:16:53 +0100

    insitialised horse class with private attributes, and implemented public getters and setters.

--- Horse.java
+++ Horse.java
@@ -8,7 +8,11 @@
 public class Horse
 {
     //Fields of class Horse
-    
+    private String name;
+    private char horseSymbol;
+    private int distanceTravelled;
+    private boolean hasFallen;
+    private double confidence;
     
       
     //Constructor of class Horse
@@ -17,60 +21,75 @@ public class Horse
      */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
+        // initialise instance variables
+        this.horseSymbol = horseSymbol;
+        this.name = horseName;
+        this.confidence = horseConfidence;
+        this.distanceTravelled = 0;
+        this.hasFallen = false;
        
     }
     
     
     
-    //Other methods of class Horse
-    public void fall()
-    {
-        
-    }
+    //getters and setters for the horse class.
     
     public double getConfidence()
     {
-        
+        return this.confidence;
     }
     
     public int getDistanceTravelled()
     {
-        
+        return this.distanceTravelled;
     }
     
     public String getName()
     {
-        
+        return this.name;
     }
     
     public char getSymbol()
     {
-        
+        return this.horseSymbol;
     }
     
-    public void goBackToStart()
-    {
-        
-    }
     
     public boolean hasFallen()
     {
-        
+        return this.hasFallen;
     }
 
-    public void moveForward()
-    {
-        
-    }
 
     public void setConfidence(double newConfidence)
     {
-        
+        if (newConfidence <0.0 || newConfidence > 1.0)
+        {
+            System.out.println("Confidence must be between 0.0 and 1.0");
+        }
+        else
+        {
+            this.confidence = newConfidence;
+        }
     }
     
     public void setSymbol(char newSymbol)
     {
-        
+        this.horseSymbol = newSymbol;
     }
     
+    // Other horse methods.
+    
+    public void fall()
+    {
+        this.hasFallen = true;
+    }
+    public void moveForward()
+    {
+        
+    }
+    public void goBackToStart()
+    {
+        
+    }
 }
